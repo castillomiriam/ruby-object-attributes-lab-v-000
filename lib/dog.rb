90f749b3fc46fc1bb74fe140e2bd8bb80723af17
Miriam Castillo <castillo.m.mc@gmail.com>
2018-02-27 20:36:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,12 +8,20 @@ class Dog
     @name
   end
 
+  def name=(new_name)
+    @name = new_name
+  end
+
   def initialize(breed)
     @breed = breed
   end
 
   def breed
     @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
   end
 
   end
